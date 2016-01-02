ed2a8986043c276b33828fa580f0543e0b40a827
Jia Hao <jiahaog@gmail.com>
2016-01-02 12:51:46 +0800

    Add two player game implementation

---
+++ src/js/game/implementation/twoPlayer.js
@@ -0,0 +1,11 @@
+import TicTacToe from './../board/tictactoe';
+import CliPlayer from '../players/cliPlayer';
+
+const game = new TicTacToe();
+
+
+const player0 = new CliPlayer(0, 'Player0', game);
+const player1 = new CliPlayer(1, 'Player1', game);
+
+game.registerPlayers(player0, player1);
+game.newGame();
