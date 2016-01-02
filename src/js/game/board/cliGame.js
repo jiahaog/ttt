0c461f852214a824a0f35eb213de170a8a67687e
Jia Hao <jiahaog@gmail.com>
2016-01-02 15:31:11 +0800

    Update 2016-01-02T07:31:07.325Z

--- src/js/game/board/cliGame.js
+++
@@ -1,9 +0,0 @@
-import TicTacToe from './tictactoe';
-import HumanPlayer from '../players/cliPlayer';
-import PerfectPlayer from '../players/perfectPlayer';
-
-const game = new TicTacToe(new HumanPlayer(0, 'Human'), new PerfectPlayer(1, 'AI'));
-game.start(winner => {
-    console.log(`Game over, winner: ${winner}`);
-    game.showBoard();
-});
